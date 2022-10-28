0dcd00c4807b7ecb5ef104c19b8d9d2bf2873e4e
Isl0m <islomrustam09@gmail.com>
2022-10-28 21:57:57 +0500

    fix deploy bug

--- app/api/api.helpers.ts
+++ app/api/api.helpers.ts
@@ -2,10 +2,7 @@ export const getContentType = () => ({
     'Content-Type':'application/json',
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods':'GET,OPTIONS,PATCH,DELETE,POST,PUT',
 
-  'Access-Control-Allow-Headers':
-  'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
 })
 
 export const errorCatch = (error:any):string  => error.response
