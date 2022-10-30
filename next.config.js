0a2d938f7f8b308b2638d03f56757a19aa320725
Isl0m <islomrustam09@gmail.com>
2022-10-30 16:20:22 +0500

    small fix

--- next.config.js
+++ next.config.js
@@ -14,32 +14,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: `https://links-store-api.vercel.app/:path*`,
+				destination: `https://links-store-api.herokuapp.com/:path*`,
 			},
 			{
 				source: '/uploads/:path*',
-				destination: `https://links-store-api.vercel.app/uploads/:path*`,
-			},
-		]
-	},
-	async headers() {
-		return [
-			{
-				// matching all API routes
-				source: '/api/:path*',
-				headers: [
-					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
-					{ key: 'Access-Control-Allow-Origin', value: '*' },
-					{
-						key: 'Access-Control-Allow-Methods',
-						value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-					},
-					{
-						key: 'Access-Control-Allow-Headers',
-						value:
-							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-					},
-				],
+				destination: `https://links-store-api.herokuapp.com/:path*`,
 			},
 		]
 	},
